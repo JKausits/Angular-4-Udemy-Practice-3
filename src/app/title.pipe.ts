05f7a0140ba7afaa4aea359ef7cc5c23b96f8657
Joe Kausits <JoeKausits@gmail.com>
2017-08-22 15:32:07 -0400

    Fixed bug with the word 'of' at the beginning of the title

--- src/app/title.pipe.ts
+++ src/app/title.pipe.ts
@@ -8,7 +8,7 @@ export class TitlePipe implements PipeTransform {
     transform(value: string) {
         const valueArr = value.split(' ').map((word, index) => {
             word = word.toLowerCase();
-            if (word === 'the' && index !== 0 || word === 'of') {
+            if (word === 'the' && index !== 0 || word === 'of' && index !== 0) {
                 return word;
             }
             return word.charAt(0).toUpperCase() + word.slice(1);

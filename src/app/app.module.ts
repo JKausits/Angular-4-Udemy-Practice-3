32c193bf9d889261ca5e9bb81bf92d61530a0b6a
Joe Kausits <JoeKausits@gmail.com>
2017-08-22 15:29:24 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { TitlePipe } from './title.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitlePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/title.pipe.ts
@@ -0,0 +1,19 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+    name: 'title'
+})
+
+export class TitlePipe implements PipeTransform {
+    transform(value: string) {
+        const valueArr = value.split(' ').map((word, index) => {
+            word = word.toLowerCase();
+            if (word === 'the' && index !== 0 || word === 'of') {
+                return word;
+            }
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        });
+
+        return valueArr.join(' ');
+    }
+}
